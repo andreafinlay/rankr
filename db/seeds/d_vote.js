963830f8fa310cc1658e97e204980099823f2b27
Aden Collinge <aden.collinge@gmail.com>
2018-05-02 22:51:57 +0000

    delete comments

--- db/seeds/d_vote.js
+++ db/seeds/d_vote.js
@@ -10,11 +10,3 @@ exports.seed = function(knex, Promise) {
       ]);
     });
 };
-
-
- // knex('poll').insert({id:1,creator_id: 1, admin_url: 'www.example.com', poll_url: 'www.example.com', open: true, question_string: "what on food??"}),
-        // knex('option').insert({id: 1, option_name:'wtih cheese', poll_id: 1}),
-        // knex('option').insert({id: 2, option_name:'wtihout cheese', poll_id: 1}),
-
-        // knex('vote').insert({id: 1, voter_name:'Mike', option_id: 1, rank: 0 }),
-        // knex('vote').insert({id: 2, voter_name:'Mike', option_id: 2, rank: 1 })
