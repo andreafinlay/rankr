e2b61c645d57a5b73d1621f7458d09afa667b5a8
Aden Collinge <aden.collinge@gmail.com>
2018-05-02 22:47:10 +0000

    adds seed files

---
+++ db/seeds/d_vote.js
@@ -0,0 +1,20 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('vote').truncate()
+    .then(function () {
+      return Promise.all([
+        knex('vote').insert({id: 1, voter_name:'Mike', option_id: 1, rank: 0 }),
+        knex('vote').insert({id: 2, voter_name:'Mike', option_id: 2, rank: 1 })
+
+      ]);
+    });
+};
+
+
+ // knex('poll').insert({id:1,creator_id: 1, admin_url: 'www.example.com', poll_url: 'www.example.com', open: true, question_string: "what on food??"}),
+        // knex('option').insert({id: 1, option_name:'wtih cheese', poll_id: 1}),
+        // knex('option').insert({id: 2, option_name:'wtihout cheese', poll_id: 1}),
+
+        // knex('vote').insert({id: 1, voter_name:'Mike', option_id: 1, rank: 0 }),
+        // knex('vote').insert({id: 2, voter_name:'Mike', option_id: 2, rank: 1 })
