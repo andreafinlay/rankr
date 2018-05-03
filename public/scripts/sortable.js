b691e334d53ab0d960cb9d4df8fb2fe65657fab0
Andrea <finlayandi@gmail.com>
2018-05-03 20:25:24 +0000

    experimenting w editing options

--- public/scripts/sortable.js
+++ public/scripts/sortable.js
@@ -1,19 +1,52 @@
  $(() => {
-   let sortTrue = document.getElementById('sortTrue');
-   let listItem = document.getElementById('item1')
-   Sortable.create(sortTrue, {
+   let pollContainer = document.querySelector('#pollContainer');
+   let pollOption = document.querySelector('.pollOption')
+
+   Sortable.create(pollContainer, {
      filter: ".js-remove",
      onFilter: function (evt) {
       var item = evt.item,
  			ctrl = evt.target;
- 		   if (Sortable.utils.is(ctrl, ".js-remove")) {  // Click on remove button
- 			item.parentNode.removeChild(listItem); // remove sortable item
+ 		   if (Sortable.utils.is(ctrl, ".js-remove")) {
+ 			item.parentNode.removeChild(pollOption);
  		       }
- 		    else if (Sortable.utils.is(ctrl, ".js-edit")) {  // Click on edit link
- 		} var el = editableList.closest(evt.item);
-    el && el.parentNode.removeChild(el);
-  }
-})
+         }
+       })
+
+     //   function createNewPollOption() {
+     //       let $tweet   = $("<div>").addClass("")
+     // 
+     //       const header = $("<header>")
+     //       $("<span>").addClass("handle").text(tweetObject.user.handle).appendTo(header);
+     //       $("<img>").attr('src', tweetObject.user.avatars.small).appendTo(header);
+     //       $("<h2>").addClass("name").text(tweetObject.user.name).appendTo(header);
+     //       $tweet.append(header);
+     //
+     //       $("<p>").text(tweetObject.content.text).appendTo($tweet);
+     //
+     //       const footer = $("<footer>")
+     //       const icons  = $("<span>").addClass("icons")
+     //       $("<i>").addClass("fas fa-heart").appendTo(icons);
+     //       $("<i>").addClass("fas fa-retweet").appendTo(icons);
+     //       $("<i>").addClass("fas fa-flag").appendTo(icons);
+     //       footer.append(icons);
+     //       $("<p>").addClass("time-stamp").text(moment.utc(tweetObject.created_at).fromNow()).appendTo(footer);
+     //       $tweet.append(footer);
+     //
+     //       return $tweet;
+     //   };
+     //
+     //   $.ajax({
+     //     method: "POST",
+     //     url: "/tweets/",
+     //     data: data
+     //   }).done(function (e) {
+     //     $("#tweets").prepend(createTweetElement(e));
+     //     $("form")[0].reset();
+     //     $(".counter").text(140);
+     //   }).fail(function (e) {
+     //     $(".new-tweet .error").text($errMsg)})
+     // });
 });
 
 
