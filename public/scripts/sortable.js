5a9950d1f2e03d4edc69387d1593e7ae39c11144
Andrea <finlayandi@gmail.com>
2018-05-03 19:30:13 +0000

    adds edit and delet buttons, not functional

--- public/scripts/sortable.js
+++ public/scripts/sortable.js
@@ -1,11 +1,31 @@
  $(() => {
-var sortTrue = document.getElementById('sortTrue');
-// console.log(el);
-Sortable.create(sortTrue, {
-  group: "sorting",
-  sort: true
+   let sortTrue = document.getElementById('sortTrue');
+   let listItem = document.getElementById('item1')
+   Sortable.create(sortTrue, {
+     filter: ".js-remove",
+     onFilter: function (evt) {
+      var item = evt.item,
+ 			ctrl = evt.target;
+ 		   if (Sortable.utils.is(ctrl, ".js-remove")) {  // Click on remove button
+ 			item.parentNode.removeChild(listItem); // remove sortable item
+ 		       }
+ 		    else if (Sortable.utils.is(ctrl, ".js-edit")) {  // Click on edit link
+ 		} var el = editableList.closest(evt.item);
+    el && el.parentNode.removeChild(el);
+  }
+})
 });
-});
+
+
+// Sortable.create(sortTrue, {
+//   group: "sorting",
+//   sort: true
+// });
+
+
+
+
+// });
 
 // var editableList = Sortable.create(editable, {
 //   filter: '.js-remove',
